55165b814a5fcd042b0983e31b30a4fcd238bd1d
Xingye Zhou <xingyechou@gmail.com>
2024-10-24 16:56:25 +0800

    Revert back to CLI only tests

--- src/test/java/seedu/address/ui/HelpWindowTest.java
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -1,31 +1,21 @@
 package seedu.address.ui;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.framework.junit5.Start;
 
-import javafx.stage.Stage;
+public class HelpWindowTest {
 
-
-public class HelpWindowTest extends ApplicationTest {
-
-    private HelpWindow helpWindow;
-    @Start
-    public void start(Stage primaryStage) throws Exception {
-        helpWindow = new HelpWindow();
-    }
+    public static final String USER_GUIDE = "https://ay2425s1-cs2103t-w13-1.github.io/tp/UserGuide.html";
+    public static final String HELLP_MESSAGE = "https://ay2425s1-cs2103t-w13-1.github.io/tp/UserGuide.html";
 
     @Test
     public void checkUserGuideUrl() {
-        assertEquals("https://ay2425s1-cs2103t-w13-1.github.io/tp/UserGuide.html", HelpWindow.USERGUIDE_URL);
+        assertEquals(USER_GUIDE, HelpWindow.USERGUIDE_URL);
     }
 
     @Test
-    public void checkOpenUrlInBrowser() {
-        assertDoesNotThrow(() -> {
-            helpWindow.isShowing();
-        });
+    public void checkHelpMessage() {
+        assertEquals("Refer to the user guide: https://ay2425s1-cs2103t-w13-1.github.io/tp/UserGuide.html",
+                HelpWindow.HELP_MESSAGE);
     }
 }
